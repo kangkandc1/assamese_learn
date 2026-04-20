880d5aba7f18199da691d2ee083373c4f506375c
kangkandc1 <kangkan.dc1@gmail.com>
2026-04-20 18:50:00 +0200

    lesson 3

--- src/data/lessons.js
+++ src/data/lessons.js
@@ -2,6 +2,7 @@
 const lessons = [
   { id: "lesson1", title: "Lektion 1 – Begrüßung", file: () => import("./lesson1.json") },
   { id: "lesson2", title: "Lektion 2 – ich bin", file: () => import("./lesson2.json") },
+  { id: "lesson3", title: "Lektion 3 – wie gehts", file: () => import("./lesson3.json") }
 ];
 
 export default lessons;
