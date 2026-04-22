841cd31d2ba594ee959bba21ecb5172eeca0c74c
kangkandc1 <kangkan.dc1@gmail.com>
2026-04-22 19:17:31 +0200

    personal pronouns

--- src/data/grammar.js
+++ src/data/grammar.js
@@ -1,6 +1,7 @@
 const grammar = [
   { id: "Grammatik-1", title: "Grammatik-W Worter", file: () => import("./grammar/grammar1.json") },
-  { id: "Grammatik-2", title: "Grammatik-Wer,Wen, Wem", file: () => import("./grammar/grammar2.json") }
+  { id: "Grammatik-2", title: "Grammatik-Wer,Wen, Wem", file: () => import("./grammar/grammar2.json") },
+  { id: "Grammatik-3", title: "Personalpronomen", file: () => import("./grammar/grammar3.json") }
 ];
 
 export default grammar;
