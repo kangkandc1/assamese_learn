979d4669ea1af2da3fc2d8067130a0308ba5a579
kangkandc1 <kangkan.dc1@gmail.com>
2026-05-04 17:38:23 +0200

    future tense

--- src/data/conjugationtables.js
+++ src/data/conjugationtables.js
@@ -1,6 +1,7 @@
 const conjugationtables = [
   { id: "table-1", title: "Basis Verben", file: () => import("./cojnugationtables/table1.json") },
-  
+  { id: "table-2", title: "Modal Verben", file: () => import("./cojnugationtables/table2.json") },
+  { id: "table-3", title: "Futur 1", file: () => import("./cojnugationtables/table3.json") },
 
 ];
 
