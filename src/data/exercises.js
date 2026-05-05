0342ad11f9f46b055daf8d04af40eb8c5bf64539
kangkandc1 <kangkan.dc1@gmail.com>
2026-05-05 14:05:41 +0200

    dativ, exercise 2

--- src/data/exercises.js
+++ src/data/exercises.js
@@ -1,5 +1,6 @@
 const exercises = [
-  { id: "exercise1", title: "Übung – Lektion 1 bis 4", file: () => import("./exercises/exercise1.json") }
+  { id: "exercise1", title: "Übung1 – Lektion 1 bis 4", file: () => import("./exercises/exercise1.json") },
+  { id: "exercise2", title: "Übung2 – Lektion 1 bis 4", file: () => import("./exercises/exercise2.json") }
 ];
 
 export default exercises;
--- src/data/grammar.js
+++ src/data/grammar.js
@@ -2,7 +2,8 @@ const grammar = [
   { id: "Grammatik-1", title: "Grammatik-W Worter", file: () => import("./grammar/grammar1.json") },
   { id: "Grammatik-2", title: "Grammatik-Wer,Wen, Wem", file: () => import("./grammar/grammar2.json") },
   { id: "Grammatik-3", title: "Personalpronomen", file: () => import("./grammar/grammar3.json") },
-{ id: "Grammatik-4", title: "Verb Konjugation", file: () => import("./grammar/grammar4.json") }
+{ id: "Grammatik-4", title: "Verb Konjugation", file: () => import("./grammar/grammar4.json") },
+{ id: "Grammatik-5", title: "Dativ", file: () => import("./grammar/grammar5.json") }
 
 ];
 
